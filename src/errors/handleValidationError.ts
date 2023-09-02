23a6cbff9bb1d3797100313c9e438e1a038472c8
AmtTawsik <amttawsik.cse@gmail.com>
2023-09-02 16:47:40 +0600

    some edit in error folder

--- src/errors/handleValidationError.ts
+++ src/errors/handleValidationError.ts
@@ -1,13 +1,18 @@
-import { Prisma } from '@prisma/client';
+import mongoose from 'mongoose';
 import { IGenericErrorResponse } from '../interfaces/common';
+import { IGenericErrorMessage } from '../interfaces/error';
 
 const handleValidationError = (
-  error: Prisma.PrismaClientValidationError
+  error: mongoose.Error.ValidationError
 ): IGenericErrorResponse => {
-  const errors = [{
-    path: "",
-    message: error.message,
-  }]
+  const errors: IGenericErrorMessage[] = Object.values(error.errors).map(
+    (el: mongoose.Error.ValidatorError | mongoose.Error.CastError) => {
+      return {
+        path: el?.path,
+        message: el?.message,
+      };
+    }
+  );
   const statusCode = 400;
   return {
     statusCode,
